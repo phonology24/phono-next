f71c5f4b24f2279c6a598a240cd53cd43772c9c4
Athul Johney <athuljonnie93@gmail.com>
2024-07-24 11:18:20 +0530

    png error

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
   },
   webpack: (config, { isServer }) => {
     config.module.rules.push({
-      test: /\.(mp4|webm|ogg|swf|ogv|png)$/,
+      test: /\.(mp4|webm|ogg|swf|ogv)$/,
       use: {
         loader: 'file-loader',
         options: {
