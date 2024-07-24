7139ca138cea21bd4f952747b43772273912882d
Athul Johney <athuljonnie93@gmail.com>
2024-07-24 12:35:12 +0530

    animation

--- components/Unified-Communications/Communications-Header.jsx
+++ components/Unified-Communications/Communications-Header.jsx
@@ -6,21 +6,21 @@ import Image from 'next/image';
 const CommunicationsHeader = () => {
   return (
     <div className="communications-header">
-  
+
 
       <div className="communications-grid communications-grid-middle communications-grid-gap-large">
         <div className="communications-content">
           <div className="communications-content-inner">
             <h1 className="communications-display-4">
-            One Platform, Endless Possibilities: Unified Communication Provider &nbsp;
+              One Platform, Endless Possibilities: Unified Communication Provider &nbsp;
               <span className="communications-gradient-ltr">
-              - UCaaS
+                - UCaaS
               </span>
             </h1>
             <p className="communications-large">
-            Unified communications streamline voice, video, messaging, and collaboration tools into a single platform. As a leading unified communication provider,
-             we offer integrated solutions that enhance efficiency, foster teamwork, and simplify communication for businesses, ensuring seamless connectivity across all channels.
-</p>
+              Unified communications streamline voice, video, messaging, and collaboration tools into a single platform. As a leading unified communication provider,
+              we offer integrated solutions that enhance efficiency, foster teamwork, and simplify communication for businesses, ensuring seamless connectivity across all channels.
+            </p>
             <ul className="communications-list">
 
               <li>
@@ -37,12 +37,12 @@ const CommunicationsHeader = () => {
         <div className="communications-photo">
           <div className="communications-photo-inner">
             <Image className="communications-photo-image" src={YealinkPhone} alt="yealinkphone" style={{
-            width: '100%',
-            height: 'auto',
-            borderRadius: '8px',
-            margin: '20px 0',
-            objectFit: 'cover',
-          }} ></Image>
+              width: '100%',
+              height: 'auto',
+              borderRadius: '8px',
+              margin: '20px 0',
+              objectFit: 'cover',
+            }} ></Image>
           </div>
         </div>
       </div>
