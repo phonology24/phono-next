1d214214db8d0c671517056648b1469eb6639529
Athul Johney <athuljonnie93@gmail.com>
2024-07-25 15:04:47 +0530

    CTA buttons

--- components/CampaignComponents/Campaign1/FreetrialComponent.jsx
+++ components/CampaignComponents/Campaign1/FreetrialComponent.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import '../../../styles/campaign/freetrialComponent.css'
+import NearMeOutlinedIcon from '@mui/icons-material/NearMeOutlined';
 
 const FreetrialComponent = () => {
   return (
     <div>
       <div className="freetrialcontainer">
+        <p className='Headingvoipform'>Need we say more?</p>
+        <div className='listfreetrial'>
+          <p><NearMeOutlinedIcon/>&nbsp;Instant setup</p>
+          <p><NearMeOutlinedIcon/>&nbsp;Real-time reporting</p>
+          <p><NearMeOutlinedIcon/>&nbsp;CRM integrations</p>
+          <p><NearMeOutlinedIcon/>&nbsp;Cloud telephony suite</p>
+          <p><NearMeOutlinedIcon/>&nbsp;99.99% uptime</p>
+          <p><NearMeOutlinedIcon/>&nbsp;Market-friendly pricing</p>
+        </div>
         <button className='freeTrialButtonCampaign'>
             Get A Free Trial
         </button>
