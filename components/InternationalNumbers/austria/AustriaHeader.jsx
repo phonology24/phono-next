82dacd75c597fa15711d4c81a6c4681b8c6a8895
Athul Johney <athuljonnie93@gmail.com>
2024-08-01 18:08:48 +0530

    build error fix4

--- components/InternationalNumbers/austria/AustriaHeader.jsx
+++ components/InternationalNumbers/austria/AustriaHeader.jsx
@@ -2,26 +2,26 @@
 import React from "react";
 import Image from "next/image";
 import ImageForInternationalNumber from "../../../public/Assets/blurry-gradient-haikei.png";
-import "../../../styles/InternationalNumbersCss/internationalheader.css";
+import styles from "../../../styles/InternationalNumbersCss/internationalheader.module.css"; // Updated to use CSS modules
 
 const AustriaHeader = () => {
   let countryName = "Austria";
   let countryFlag = "https://flagsapi.com/AT/flat/64.png";
 
   return (
-    <div className="InternationalHeaderContainer">
-      <div className="InternationalContent">
-        <h1 className="Heading">
-          Get <span className="gradientLetter">{countryName} </span>
+    <div className={styles.InternationalHeaderContainer}>
+      <div className={styles.InternationalContent}>
+        <h1 className={styles.Heading}>
+          Get <span className={styles.gradientLetter}>{countryName} </span>
           Virtual Phone Number
         </h1>
         <h2>Engage With {countryName} Customers and Prospects, Efficiently.</h2>
-        <button className="InternationalButton">Start Demo</button>
+        <button className={styles.InternationalButton}>Start Demo</button>
         {/* Uncomment to use the image */}
         {/* <Image 
           src={ImageForInternationalNumber} 
           alt="Background" 
-          className="flagImage"
+          className={styles.flagImage}
         /> */}
       </div>
       {/* Uncomment if you want to add a visual element */}
