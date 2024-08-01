d4cfbee0688ffbbd708f2d4e5a798e6c4c92f82d
Athul Johney <athuljonnie93@gmail.com>
2024-08-01 16:59:17 +0530

    build error fix

--- components/InternationalNumbers/austria/AustriaHeader.jsx
+++ components/InternationalNumbers/austria/AustriaHeader.jsx
@@ -1,33 +1,33 @@
-"use client"
-import React from 'react';
-import styles from '../../../styles/InternationalNumbersCss/internationalHeader.module.css';
-import Image from 'next/image';
-import ImageForInternationalNumber from '../../../public/Assets/blurry-gradient-haikei.png';
+"use client";
+import React from "react";
+import Image from "next/image";
+import ImageForInternationalNumber from "../../../public/Assets/blurry-gradient-haikei.png";
+import "../../../styles/InternationalNumbersCss/internationalHeader.css";
 
 const AustriaHeader = () => {
-    let countryName = "Austria";
-    let countryFlag = "https://flagsapi.com/AT/flat/64.png";
+  let countryName = "Austria";
+  let countryFlag = "https://flagsapi.com/AT/flat/64.png";
 
-    return (
-        <div className={styles.InternationalHeaderContainer}>
-            <div className={styles.InternationalContent}>
-                
-            <h1 className={styles.Heading}>
-                
-                    Get&nbsp;<span className={styles.gradientLetter}>{countryName}&nbsp;</span> 
-                    Virtual Phone Number
-                 
-                </h1>                <h2>Engage With {countryName} Customers and Prospects, Efficiently.</h2>
-                <button className={styles.InternationalButton}>Start Demo</button>
-                {/* <Image 
-                    src={ImageForInternationalNumber} 
-                    alt="Background" 
-                    className={styles.flagImage}
-                /> */}
-            </div>
-            {/* <div className={styles.visualElement}></div> */}
-        </div>
-    );
-}
+  return (
+    <div className="InternationalHeaderContainer">
+      <div className="InternationalContent">
+        <h1 className="Heading">
+          Get <span className="gradientLetter">{countryName} </span>
+          Virtual Phone Number
+        </h1>
+        <h2>Engage With {countryName} Customers and Prospects, Efficiently.</h2>
+        <button className="InternationalButton">Start Demo</button>
+        {/* Uncomment to use the image */}
+        {/* <Image 
+          src={ImageForInternationalNumber} 
+          alt="Background" 
+          className="flagImage"
+        /> */}
+      </div>
+      {/* Uncomment if you want to add a visual element */}
+      {/* <div className="visualElement"></div> */}
+    </div>
+  );
+};
 
 export default AustriaHeader;
