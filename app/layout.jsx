8fa41fdff1c816c9f97aaadcbb575fc45cc16cf3
Athul Johney <athuljonnie93@gmail.com>
2024-08-09 14:42:18 +0530

    Content change HOMEPAGE

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,13 +2,14 @@ import Navbar from '../components/Navbar';
 // import Footer from '../components/Footer';
 import TopContactBar from '@/components/TopContactBar';
 import '../styles/globals.css'; // Ensure your global styles are imported
+import Head from 'next/head';
 
 export default function Layout({ children }) {
   return (
     <html lang="en">
-      <head>
-      <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
-      </head>
+    <Head>
+    </Head>
+    <link rel="icon" href="/favicon.ico" />
       <body>
         <TopContactBar />
         <Navbar />
