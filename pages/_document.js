8fe9542a30dc05a739b85b37a6c2ea5df5c2f6b2
Athul Johney <athuljonnie93@gmail.com>
2024-08-13 12:34:58 +0530

    SEOTRACKER2

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,11 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap" rel="stylesheet" />
           <link rel="icon" href="favicon.ico" type="image/x-icon"  />
+          <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MJFGQBVW');`,
+          }}
+        />
 
         </Head>
         <body>
