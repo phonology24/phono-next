4bf960417a97352d3f0ab848b0950f72964b9732
Athul Johney <athuljonnie93@gmail.com>
2024-08-21 17:39:26 +0530

    analytics

--- app/voip-form/page.jsx
+++ app/voip-form/page.jsx
@@ -12,36 +12,52 @@ export const metadata = {
     canonical: 'https://www.phonology.io/voip-form',
   },
   robots: 'index, follow, max-snippet:-1, max-video-preview:-1, max-image-preview:large', // Robots meta tag
-
 };
 
-
 const Page = () => {
-
-
   return (
     <>
-          <script
-            type="text/javascript"
-            id="zsiqchat"
-            dangerouslySetInnerHTML={{
-              __html: `
-                var $zoho=$zoho || {};
-                $zoho.salesiq = $zoho.salesiq || {
-                  widgetcode: "siqed8e902c4505673f934b08d408819a9982cdb93c35ad32b2bceabd67e6bb9e4c",
-                  values: {},
-                  ready: function() {}
-                };
-                var d=document;s=d.createElement("script");
-                s.type="text/javascript";
-                s.id="zsiqscript";
-                s.defer=true;
-                s.src="https://salesiq.zohopublic.in/widget";
-                t=d.getElementsByTagName("script")[0];
-                t.parentNode.insertBefore(s,t);
-              `,
-            }}
-          />
+      <Head>
+        {/* Add metadata, title, and other head elements */}
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <link rel="canonical" href={metadata.alternates.canonical} />
+        <meta name="robots" content={metadata.robots} />
+      </Head>
+      <script
+        type="text/javascript"
+        id="zsiqchat"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var $zoho=$zoho || {};
+            $zoho.salesiq = $zoho.salesiq || {
+              widgetcode: "siqed8e902c4505673f934b08d408819a9982cdb93c35ad32b2bceabd67e6bb9e4c",
+              values: {},
+              ready: function() {}
+            };
+            var d=document;s=d.createElement("script");
+            s.type="text/javascript";
+            s.id="zsiqscript";
+            s.defer=true;
+            s.src="https://salesiq.zohopublic.in/widget";
+            t=d.getElementsByTagName("script")[0];
+            t.parentNode.insertBefore(s,t);
+          `,
+        }}
+      />
+      {/* Google Tag Manager Script */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16678504638"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16678504638');
+          `,
+        }}
+      />
       <div>
         <Campaign1 />
       </div>
