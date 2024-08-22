5d05afd948f22768b657106572888d0f4d67fdb2
Athul Johney <athuljonnie93@gmail.com>
2024-08-22 10:43:11 +0530

    Recaptcha v3

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,8 +24,8 @@ export default function Layout({ children }) {
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MJFGQBVW');`,
           }}
         />
-        <script src='https://www.google.com/recaptcha/api.js'></script>
-      </Head>
+<script src="https://www.google.com/recaptcha/api.js?render=reCAPTCHA_6LfJjSwqAAAAACCGN5biITkYEHR4fkfXL-vhWkOI"></script>     
+ </Head>
       <link rel="icon" href="/favicon.ico" />
       <body>
         <noscript>
