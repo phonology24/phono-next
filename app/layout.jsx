28c88d537bba73bb08ba2f5e28054cf08b72340f
Athul Johney <athuljonnie93@gmail.com>
2024-08-27 15:28:39 +0530

    gtm.js3

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,44 @@
-return (
-  <html lang="en">
-    <Head>
-      {/* Google Tag Manager */}
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-T4FS3L3Q');</script>
-      {/* End Google Tag Manager */}
+"use client";
 
-      {/* reCAPTCHA */}
-      <script src="https://www.google.com/recaptcha/api.js?render=reCAPTCHA_6LfJjSwqAAAAACCGN5biITkYEHR4fkfXL-vhWkOI"></script> 
+import Navbar from '../components/Navbar';
+import TopContactBar from '@/components/TopContactBar';
+import '../styles/globals.css'; // Ensure your global styles are imported
+import Head from 'next/head';
+import { usePathname } from 'next/navigation';
 
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <body>
-      {!shouldHideNavbar && (
-        <>
-          <TopContactBar />
-          <Navbar />
-        </>
-      )}
-      {children}
-    </body>
-  </html>
-);
+export default function Layout({ children }) {
+  const pathname = usePathname();
+
+  // Routes where the navbar should not be displayed
+  const noNavbarRoutes = ['/voip-form', '/phonology-blogs'];
+
+  // Check if the current path starts with '/phonology-blogs/' or is in noNavbarRoutes
+  const shouldHideNavbar = noNavbarRoutes.includes(pathname) || pathname.startsWith('/phonology-blogs/');
+
+  // Return must be inside the component function
+  return (
+    <html lang="en">
+      <Head>
+        {/* Google Tag Manager */}
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'}); var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer' ? '&l='+l : ''; j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl; f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-T4FS3L3Q');`}
+        </script>
+        {/* End Google Tag Manager */}
+        
+        {/* reCAPTCHA */}
+        <script src="https://www.google.com/recaptcha/api.js?render=reCAPTCHA_6LfJjSwqAAAAACCGN5biITkYEHR4fkfXL-vhWkOI"></script>
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        {!shouldHideNavbar && (
+          <>
+            <TopContactBar />
+            <Navbar />
+          </>
+        )}
+        {children}
+      </body>
+    </html>
+  );
+}
