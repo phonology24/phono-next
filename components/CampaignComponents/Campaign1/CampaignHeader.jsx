1bc352503d3e67acc85433f1071b15d7a2255b70
Athul Johney <athuljonnie93@gmail.com>
2024-09-03 13:00:07 +0530

    carousel

--- components/CampaignComponents/Campaign1/CampaignHeader.jsx
+++ components/CampaignComponents/Campaign1/CampaignHeader.jsx
@@ -9,7 +9,10 @@ import EqualizerOutlinedIcon from '@mui/icons-material/EqualizerOutlined';
 import EarbudsOutlinedIcon from '@mui/icons-material/EarbudsOutlined';
 import AutorenewOutlinedIcon from '@mui/icons-material/AutorenewOutlined';
 import EastOutlinedIcon from '@mui/icons-material/EastOutlined';
+import '../../../styles/campaign/carousel.css'
 import Image from 'next/image';
+import EmblaCarousel from '../../CampaignComponents/Campaign1/carouselComps/Carousel'
+import Autoplay from 'embla-carousel-autoplay';
 
 const CampaignHeader = () => {
     const [cards, setCards] = useState([
@@ -20,6 +23,10 @@ const CampaignHeader = () => {
         { id: 6, content: 'Instant Setup', icon: <AutorenewOutlinedIcon /> },
         { id: 4, content: 'Virtual Numbers', icon: <EqualizerOutlinedIcon /> },
     ]);
+
+    const OPTIONS = { loop: true, duration: 0, Autoplay: true }
+    const SLIDE_COUNT = 5
+    const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
     return (
         <>
@@ -33,12 +40,12 @@ const CampaignHeader = () => {
                 <div className='twocards'>
                     <div className='cards left'>
                         <div className="button-div">
-                    <button className='button-86'>Voice</button>
-                    <button className='button-86'>Use Cases</button>
-                    <button className='button-86'>SIP Trunking</button>
-                    <button className='button-86'>IVR</button>
-                    {/*  <button>button 1</button> */}
-                    </div>
+                            <button className='button-86'>Voice</button>
+                            <button className='button-86'>Use Cases</button>
+                            <button className='button-86'>SIP Trunking</button>
+                            <button className='button-86'>IVR</button>
+                            {/*  <button>button 1</button> */}
+                        </div>
                         <h1>Trusted VoIP Communication for Small, Medium, and Large Enterprises.</h1>
                         <p>
                             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -56,14 +63,16 @@ const CampaignHeader = () => {
                             </div>
                         ))}
                     </div> */}
-                    <div className="cta-buttons">
-                    <button className='button-85'>Try it for free &nbsp; {`\u2192`}</button>
-                    {/* <button>Contact An Expert</button> */}
-                    </div>
+                        <div className="cta-buttons">
+                            <button className='button-85'>Try it for free &nbsp; {`\u2192`}</button>
+                            {/* <button>Contact An Expert</button> */}
+                        </div>
                     </div>
                     <div className='cards right'>
                         {/* <iframe aria-label='SIGN UP FOR A FREE DEMO' frameBorder="0" src='https://forms.zohopublic.in/phonology/form/CampaignForm/formperma/ZSZ6bo7a2Bc7eKru1S4sKmXzI4dMI8Rp-9bOSbnInZ4' className='form-frame'></iframe> */}
-                        <Image src={LandingPageImage} alt='phono-img' width={1000} height={1000} className='right-image' />
+                        {/* <Image src={LandingPageImage} alt='phono-img' width={1000} height={1000} className='right-image' /> */}
+                        <EmblaCarousel slides={SLIDES} options={OPTIONS} className='right-image' />
+
                     </div>
                 </div>
             </div>
---
+++ components/CampaignComponents/Campaign1/carouselComps/Carousel.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import Fade from 'embla-carousel-fade'
+import Carousel1 from '../../../../public/Assets/carousel1.png'
+import Carousel2 from '../../../../public/Assets/Carousel2.png'
+import Carousel3 from '../../../../public/Assets/Carousel3.png'
+
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons
+} from './EmblaCarouselArrowButtons'
+import { DotButton, useDotButton } from './EmblaCarouselDotButtons'
+import emblaCarouselAutoplay from 'embla-carousel-autoplay'
+import Image from 'next/image'
+const IMAGES = [Carousel1, Carousel2, Carousel3];
+
+const EmblaCarousel = (props) => {
+  const { slides, options } = props
+  const [emblaRef, emblaApi] = useEmblaCarousel(options,  [ Fade(),
+    emblaCarouselAutoplay({ playOnInit: true, delay: 3000 })
+  ])
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi)
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi)
+
+  return (
+    <div className="embla">
+  <div className="embla__viewport" ref={emblaRef}>
+      <div className="embla__container">
+        {IMAGES.map((image, index) => (
+          <div className="embla__slide" key={index}>
+            <Image
+              className="embla__slide__img"
+              src={image}
+              alt={`Carousel Image ${index + 1}`}
+              width={1000}
+              height={1000}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+
+      {/* <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={'embla__dot'.concat(
+                index === selectedIndex ? ' embla__dot--selected' : ''
+              )}
+            />
+          ))}
+        </div>
+      </div> */}
+    </div>
+  )
+}
+
+export default EmblaCarousel
