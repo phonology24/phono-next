f29846b653a85cf13877d3a51564f1e6124a0b4a
Athul Johney <athuljonnie93@gmail.com>
2024-09-16 12:28:44 +0530

    response correction

--- components/CampaignComponents/Campaign1/CampaignMarquee.jsx
+++ components/CampaignComponents/Campaign1/CampaignMarquee.jsx
@@ -17,12 +17,18 @@ const logos = [
   { id: 4, src: MatrixLogo, alt: 'Matrix Logo' },
   { id: 5, src: GrandstreamLogo, alt: 'Grandstream Logo' },
   { id: 6, src: PolycomLogo, alt: 'Polycom Logo' },
+  { id: 7, src: CiscoLogo, alt: 'Cisco Logo' },
+  { id: 8, src: ElastixLogo, alt: 'Elastix Logo' },
+  { id: 9, src: YeaLink, alt: 'YeaLink Logo' },
+  { id: 10, src: MatrixLogo, alt: 'Matrix Logo' },
+  { id: 11, src: GrandstreamLogo, alt: 'Grandstream Logo' },
+  { id: 12, src: PolycomLogo, alt: 'Polycom Logo' },
 ];
 
 const CampaignMarquee = () => {
   return (<>
     <h2 className='marquee-header-campaign'>Our Partners</h2>
-      <Marquee gradient={false} speed={100} style={{ zIndex: -999 }}>
+      <Marquee gradient={false} speed={100} style={{ zIndex: -999 }} className='campaign-marquee'>
       {logos.map((logo) => (
         <div className="logo-card" key={logo.id}>
           <Image
