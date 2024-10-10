2f4f2622141964454884bf8b665c98cf29648081
Athul Johney <athuljonnie93@gmail.com>
2024-10-10 11:34:48 +0530

    Testing

--- app/uifn-number/page.jsx
+++ app/uifn-number/page.jsx
@@ -1,10 +1,11 @@
 import UifnPage from '@/components/UifnPage.jsx'
+import VirtualNumber from '@/components/VirtualNumber'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-   <UifnPage/>
+   <VirtualNumber/>
     </div>
   )
 }
