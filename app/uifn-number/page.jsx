8843ca433abb31dd22dd8787311c4c3869ab1fd9
Athul Johney <athuljonnie93@gmail.com>
2024-10-10 10:43:21 +0530

    Refactor

--- app/uifn-number/page.jsx
+++ app/uifn-number/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import UifnHeader from '@/components/uifn/Uifn-header'
 import UifnTwoCards from '@/components/uifn/Uifn-twoCards'
 import UifnHowItWorks from '@/components/uifn/Uifn-howItWorks'
-import UifnPainPoints from '@/components/uifn/uifn-Points'
+import UifnPainPoints from '@/components/uifn/Uifn-Points'
 import Footer from '@/components/Footer'
 const page = () => {
   return (
--- components/uifn/uifn-header.jsx
+++ components/uifn/uifn-header.jsx
@@ -12,15 +12,20 @@ const UifnHeader = () => {
         <div className="uifn-content">
           <div className="uifn-content-inner">
             <h1 className="uifn-display-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse v
-                          <span className="uifn-gradient-ltr">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse v
+              <span className="uifn-gradient-ltr">
                 - UCaaS
               </span>
             </h1>
             <p className="uifn-large">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel est mollis, accumsan justo eu, molestie sem. Donec in nibh neque. Praesent eget orci sed nisi molestie scelerisque eget eu velit. Cras enim sem, auctor sed dui ac, vehicula pellentesque nibh. Vivamus consectetur odio hendrerit neque consectetur, quis lacinia lorem volutpat. Nam imperdiet risus in sollicitudin vestibulum. Sed eros arc
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Suspendisse vel est mollis, accumsan justo eu, molestie sem. 
+              Donec in nibh neque. Praesent eget orci sed nisi molestie scelerisque
+              eget eu velit. Cras enim sem, auctor sed dui ac, vehicula pellentesque nibh. 
+              Vivamus consectetur odio hendrerit neque consectetur, quis lacinia lorem volutpat.
+              Nam imperdiet risus in sollicitudin vestibulum. Sed eros arc
             </p>
-        
+
           </div>
         </div>
         <div className="uifn-photo">
@@ -29,7 +34,7 @@ const UifnHeader = () => {
               className="uifn-photo-image"
               src={YealinkPhone}
               alt="yealinkphone"
-        
+
             />
           </div>
         </div>
