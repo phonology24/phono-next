de2a60796181b6103a552c2b812d73ff6439e095
Athul Johney <athuljonnie93@gmail.com>
2024-10-17 12:46:39 +0530

    Uifn Content Hero

--- components/Uifn/UifnHeader.jsx
+++ components/Uifn/UifnHeader.jsx
@@ -12,18 +12,16 @@ const UifnHeader = () => {
         <div className="uifn-content">
           <div className="uifn-content-inner">
             <h1 className="uifn-display-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse v
-              <span className="uifn-gradient-ltr">
-                - UCaaS
-              </span>
+            <span className="uifn-gradient-ltr">
+            UIFN Toll-Free Numbers,             </span>
+             One Solution for Worldwide Accessibility. 
             </h1>
             <p className="uifn-large">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Suspendisse vel est mollis, accumsan justo eu, molestie sem. 
-              Donec in nibh neque. Praesent eget orci sed nisi molestie scelerisque
-              eget eu velit. Cras enim sem, auctor sed dui ac, vehicula pellentesque nibh. 
-              Vivamus consectetur odio hendrerit neque consectetur, quis lacinia lorem volutpat.
-              Nam imperdiet risus in sollicitudin vestibulum. Sed eros arc
+            A UIFN toll-free number (Universal International Freephone Number) provides businesses with a single, 
+            easy to remember contact number that works across multiple countries. It enables customers to connect
+             effortlessly, free of charge, improving global accessibility and customer satisfaction. By using a 
+             UIFN toll-free number, your business can foster trust, offer 24/7 availability, and streamline 
+             communication, ensuring international customers can reach you with ease.
             </p>
 
           </div>
