0da5cd8da09587d3e2455d5401a2e83c4a629a6d
Athul Johney <athuljonnie93@gmail.com>
2024-12-19 15:38:03 +0530

    Recommit

--- components/Home/Products.jsx
+++ components/Home/Products.jsx
@@ -25,30 +25,30 @@ const Products = () => {
         <Image src={uf} alt="AI Design Assistance"  width={1000} height={1000}/>
         <h3>Unlock seamless connectivity with SIP Trunking</h3>
           <p>
-            Unlock seamless connectivity with SIP Trunking—reliable, scalable, and cost-effective voice solutions that integrate effortlessly with your existing PBX systems, empowering global communication.
+            Unlock seamless connectivity with SIP Trunking—reliable, scalable, and cost-effective voice solutions that integrate effortlessly with your existing PBX systems, empowering global communication.
           </p>
         </div>
         <div className="feature-card">
         <Image src={vn} alt="AI Design Assistance"  width={1000} height={1000}/>
           <h3>Stay connected with Virtual Numbers</h3>
           <p>
-            Stay connected with Virtual Numbers—they are cost-effective, flexible, and perfect for delivering a seamless communication experience to your customers globally.           </p>
+            Stay connected with Virtual Numbers—they are cost-effective, flexible, and perfect for delivering a seamless communication experience to your customers globally.           </p>
         </div>
         <div className="feature-card">
         <Image src={uf} alt="AI Design Assistance"  width={1000} height={1000}/>
           <h3>Empower your business with Unified Communication</h3>
           <p>
-            Empower your business with Unified Communication—integrating all communication channels into a single, intuitive solution for ultimate efficiency          </p>
+            Empower your business with Unified Communication—integrating all communication channels into a single, intuitive solution for ultimate efficiency          </p>
         </div>
         <div className="feature-card">
         <Image src={ct} alt="AI Design Assistance"  width={1000} height={1000}/>
           <h3>Experience the power of Cloud Telephony</h3>
           <p>
-            Experience the power of Cloud Telephony—streamlined, cost-efficient, and built to keep your team and customers connected worldwide.          </p>
+            Experience the power of Cloud Telephony—streamlined, cost-efficient, and built to keep your team and customers connected worldwide.          </p>
         </div>
         <div className="feature-card">
         <Image src={wapi} alt="AI Design Assistance"  width={1000} height={1000}/>
-          <h3>Watel: Empowering Business Communication with WhatsApp API</h3>
+          <h3>Watel: Empowering Business Communication with WhatsApp API</h3>
           <p>
             Watel, our WhatsApp Business API platform, empowers your business with instant messaging, automation, and personalized customer experiences.
           </p>
