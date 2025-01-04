9ddba91de8862605640413fc34514e72a8b66d72
Athul Johney <athuljonnie93@gmail.com>
2025-01-04 12:23:19 +0530

    About us content

--- components/About/Heading.jsx
+++ components/About/Heading.jsx
@@ -11,7 +11,7 @@ const AboutHeading = () => {
     <section className="about-heading">
       <div className="about-heading__content">
         <h2 className="about-heading__title">
-          We are passionate about revolutionizing the way businesses operate through the power of technology.
+        Our Story: Connecting Businesses, Empowering Growth, and Building Relationships Worldwide.
         </h2>
       </div>
       <div className="about-heading__image-container">
@@ -32,9 +32,15 @@ const AboutHeading = () => {
         </button> */}
       </div>
       <div className="about-heading__description">
-        <h3>We have rules to create a product start from scratch</h3>
+        <h3>About Us</h3>
         <p>
-          Our products are designed with the user in mind, and are tailored to provide custom solutions for businesses of all sizes. Whether you're looking for cloud-based software or mobile applications, our products are intuitive, flexible, and designed to meet your specific needs.
+        At Phonology, we believe communication is the cornerstone of every successful business. For over 19 years,
+         we’ve been at the forefront of telecom innovation, delivering cutting-edge solutions like VoIP, cloud 
+         telephony, SIP trunks, and hosted PBX to businesses across the globe. Our mission is simple: to redefine
+          how the world communicates by providing seamless, scalable, and secure communication systems tailored
+           to your needs. With a proven track record of excellence, we’re more than a service provider—we’re
+            your trusted partner in transforming communication into a competitive advantage. Step into the 
+            future of connection with Phonology.
         </p>
       </div>
     </section>
