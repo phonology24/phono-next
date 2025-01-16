9a64bc47f6ff97c85599fab82a33e7cbc3e3e31d
Athul Johney <athuljonnie93@gmail.com>
2025-01-16 11:12:00 +0530

    guidelines

---
+++ app/contact-us/support/features-explained/optimize-network/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FeatureIndex from '@/components/ContactUs/User/FeaturesPage/FeatureIndex'
+
+const page = () => {
+  return (
+    <div>
+      <FeatureIndex />
+    </div>
+  )
+}
+
+export default page
--- components/ContactUs/User/FeaturesPage/FeatureLanding/FeaturesMain.jsx
+++ components/ContactUs/User/FeaturesPage/FeatureLanding/FeaturesMain.jsx
@@ -1,72 +1,81 @@
+"use client"
 import React from "react";
 import "./FeaturedCards.css";
 import NetworkCheckIcon from "@mui/icons-material/NetworkCheck";
 import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
 import SecurityIcon from "@mui/icons-material/Security";
+import Link from "next/link";
+import {useRouter  } from "next/navigation";
 
 const FeaturedCards = () => {
+    const router = useRouter();
     const cards = [
         {
             title: "Optimize Network",
-            content: `Improve network efficiency by leveraging advanced analytics to identify bottlenecks and ensure seamless connectivity for customers. Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.`,
+            content: `Improve network efficiency by leveraging advanced analytics to identify bottlenecks and ensure seamless connectivity for customers.`,
             icon: <NetworkCheckIcon className="featuredCards-muiIcon" />,
-            // Uncomment the following line if you want to use an image instead of an icon
             image: "/Assets/australia.png",
+            url: "features-explained/optimize-network",
         },
         {
             title: "Enhance Coverage",
-            content: `Deploy cutting-edge solutions to expand network coverage in remote and underserved areas, ensuring accessibility for everyone. Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.`,
+            content: `Deploy cutting-edge solutions to expand network coverage in remote and underserved areas, ensuring accessibility for everyone.`,
             icon: <SignalCellularAltIcon className="featuredCards-muiIcon" />,
-            // Uncomment the following line if you want to use an image instead of an icon
             image: "/Assets/brazil.png",
+            url: "features-explained/optimize-network",
         },
         {
             title: "Boost Security",
-            content: `Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.`,
+            content: `Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.`,
             icon: <SecurityIcon className="featuredCards-muiIcon" />,
-            // Uncomment the following line if you want to use an image instead of an icon
             image: "/Assets/belgium.png",
+            url: "features-explained/optimize-network",
         },
         {
             title: "Boost Security",
-            content: `Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.`,
+            content: `Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.`,
             icon: <SecurityIcon className="featuredCards-muiIcon" />,
-            // Uncomment the following line if you want to use an image instead of an icon
-            image: "/Assets/croatia.png",
+            image: "/Assets/belgium.png",
+            url: "features-explained/optimize-network",
         },
         {
             title: "Boost Security",
-            content: `Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.`,
+            content: `Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.`,
             icon: <SecurityIcon className="featuredCards-muiIcon" />,
-            // Uncomment the following line if you want to use an image instead of an icon
-            image: "/Assets/greece.png",
+            image: "/Assets/belgium.png",
+            url: "features-explained/optimize-network",
         },
         {
             title: "Boost Security",
-            content: `Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.`,
+            content: `Implement robust security protocols to safeguard customer data and prevent unauthorized access across telecom networks.`,
             icon: <SecurityIcon className="featuredCards-muiIcon" />,
-            // Uncomment the following line if you want to use an image instead of an icon
-            image: "/Assets/belize.png",
+            image: "/Assets/belgium.png",
+            url: "features-explained/optimize-network",
         },
     ];
+
+const redirector = (url)=> {
+    router.push(url)
+}
 
     return (
         <div className="featuredCards-container">
             {cards.map((card, index) => (
-                <div className="featuredCards-card" key={index}>
-                    <div className="featuredCards-icon">
-                        {/* Check if an image is provided, otherwise use the MUI icon */}
-                        {card.image ? (
-                            <img src={card.image} alt={card.title} className="featuredCards-image" />
-                        ) : (
-                            card.icon
-                        )}
+                <div className="featuredCards-card" style={{ cursor: "pointer" }}>
+                        {/* <Link href={card.url} key={index} passHref> */}
+                        <div className="featuredCards-icon">
+                            {card.image ? (
+                                <img src={card.image} alt={card.title} className="featuredCards-image" />
+                            ) : (
+                                card.icon
+                            )}
+                        </div>
+                        <div className="featuredCards-content">
+                            <h3 className="featuredCards-title" onClick={() => redirector( card.url)}>{card.title}</h3>
+                            <p className="featuredCards-text">{card.content}</p>
+                        </div>
+                {/* </Link> */}
                     </div>
-                    <div className="featuredCards-content">
-                        <h3 className="featuredCards-title">{card.title}</h3>
-                        <p className="featuredCards-text">{card.content}</p>
-                    </div>
-                </div>
             ))}
         </div>
     );
