09903ca45b4917b965c2bc34036ca5f00fc8f9ff
Athul Johney <athuljonnie93@gmail.com>
2025-02-18 12:22:51 +0530

    Image Update For Call Transport

--- components/ContactUs/User/FeaturesPage/CallTransferFeaturesPage/CallTransferUseCases.jsx
+++ components/ContactUs/User/FeaturesPage/CallTransferFeaturesPage/CallTransferUseCases.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import '../VoicemailFeaturesPage/VoicemailUseCases.css'; // Importing the CSS file for styling
 import Image from "next/image";
-import image from "../../../../../public/Assets/belgium.png"
-const CallTransferUseCases = () => {
+import image from "../../../../../public/Assets/col.jpg"
+import image1 from "../../../../../public/Assets/peeps.jpg"
+import image2 from "../../../../../public/Assets/doc-on-phone.jpg"
+import image3 from "../../../../../public/Assets/team-collab.jpg"const CallTransferUseCases = () => {
     return (
         <>
             <h4 className="usecase-h4">
@@ -24,7 +26,7 @@ const CallTransferUseCases = () => {
                     </p>
                 </div>
                 <div className="usecase-card row">
-                    <Image className="usecase-image" src={image} alt="Shop" width={400} height={500} />
+                    <Image className="usecase-image" src={image1} alt="Shop" width={400} height={500} />
                     <div>
                         <div className="usecase-header">
                             {/* <h4>Payment</h4> */}
@@ -40,7 +42,7 @@ const CallTransferUseCases = () => {
                     </div>
                 </div>
                 <div className="usecase-card row">
-                    <Image className="usecase-image" src={image} alt="Shop" width={400} height={500} />
+                    <Image className="usecase-image" src={image2} alt="Shop" width={400} height={500} />
                     <div>
                         <div className="usecase-header">
                             {/* <h4>Experience</h4> */}
@@ -59,7 +61,7 @@ const CallTransferUseCases = () => {
                     <div className="usecase-header">
                         {/* <h4>Rating</h4> */}
                     </div>
-                    <Image className="usecase-image" src={image} alt="Shop" width={500} height={500} />
+                    <Image className="usecase-image" src={image3} alt="Shop" width={500} height={500} />
                     <h3 className="usecase-title">Team Collaboration in Distributed Workplaces
 
                     </h3>
