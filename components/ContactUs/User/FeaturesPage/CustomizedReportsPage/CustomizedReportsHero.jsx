604b78dbec6531b991eba8876842f6a1b6c82126
Athul Johney <athuljonnie93@gmail.com>
2025-02-20 13:22:30 +0530

    Hero image updates

--- components/ContactUs/User/FeaturesPage/CustomizedReportsPage/CustomizedReportsHero.jsx
+++ components/ContactUs/User/FeaturesPage/CustomizedReportsPage/CustomizedReportsHero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../VoicemailFeaturesPage/VoiceMailFeaturesHero.css'; // Importing the CSS file for styling
 import Image from 'next/image';
-import HeroImage from "/public/Assets/home-hero3.png"
+import HeroImage from "../../../../../public/Assets/CCR.png"
 
 const CustomizedReportsHero = () => {
   return (
